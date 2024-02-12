0c7d30e66816418503402a44928bc32475e4d6f0
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-02-13 08:28:23 +0900

    リストに対応

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,6 +1,6 @@
 chrome.storage.local.get(['isBlur'], function (result): void {
     if (result.isBlur) {
-        document.querySelectorAll<HTMLElement>('h1,h2,h3,p,a,span').forEach((element): void => {
+        document.querySelectorAll<HTMLElement>('h1,h2,h3,p,a,span,ul,li').forEach((element): void => {
             element.style.filter = 'blur(5px)';
 
             element.addEventListener('mouseover', function (): void {
